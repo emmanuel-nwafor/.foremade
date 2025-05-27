fb3b0a3819d687fce01df9d325efa8f421b15f2f
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-05-26 23:41:18 -0700

    all commit

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -84,21 +84,8 @@ const Carousel = () => {
           <img
             src={slide.image}
             alt={`Slide ${index}`}
-            className="w-full h-full object-cover"
+            className="w-full"
           />
-          {/* <div className="absolute inset-0 flex items-center justify-start p-2 sm:p-4 md:p-6 lg:p-8 bg-black bg-opacity-30">
-            <div className="text-white max-w-[180px] sm:max-w-xs md:max-w-sm lg:max-w-md text-left">
-              <h2 className="text-md sm:text-base md:text-lg lg:text-xl xl:text-2xl font-bold mb-1 sm:mb-2">
-                {slide.text}
-              </h2>
-              <p className="text-[10px] sm:text-xs md:text-sm lg:text-base mb-2 sm:mb-3">
-                {slide.description}
-              </p>
-              <button className="bg-white text-gray-800 px-3 sm:px-4 md:px-5 py-1 sm:py-1.5 md:py-2 rounded-full hover:bg-blue-300 hover:text-white transition-all text-[10px] sm:text-xs md:text-sm">
-                {slide.button}
-              </button>
-            </div>
-          </div> */}
         </div>
       ))}
     </div>

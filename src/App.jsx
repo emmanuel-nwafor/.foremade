19a4e7ad99ebed7319c050a5face75f564fe532e
E-bethel <abundanceezeala@gmail.com>
2025-08-07 01:12:56 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -34,18 +34,18 @@ import SellerProductGallery from './seller/SellerProductGallery';
 import Support from './pages/Support';
 import TermsConditions from './pages/TermsAndConditions';
 
-import Admin from './admin/Admin';
-import AdminDashboard from './admin/AdminDashboard';
-import AdminNotifications from './admin/AdminNotifications';
-import AdminUsers from './admin/AdminUsers';
-import AdminPayoutMonitor from './admin/AdminPayoutMonitor';
-import AdminEditBannerAndOthers from './admin/AdminEditBannerAndOthers';
-import AdminEditDeals from './admin/AdminEditDeals';
-import AdminEditFees from './admin/AdminEditFees';
-import AdminCategoryEdit from './admin/AdminCategoryEdit';
-import AdminManager from './admin/AdminManager';
-import AdminSellerWallet from './admin/AdminSellerWallet';
-import AdminProSellerRequests from './admin/AdminProSellerRequests'
+import Admin from './Admin/Admin';
+import AdminDashboard from './Admin/AdminDashboard';
+import AdminNotifications from './Admin/AdminNotifications';
+import AdminUsers from './Admin/AdminUsers';
+import AdminPayoutMonitor from './Admin/AdminPayoutMonitor';
+import AdminEditBannerAndOthers from './Admin/AdminEditBannerAndOthers';
+import AdminEditDeals from './Admin/AdminEditDeals';
+import AdminEditFees from './Admin/AdminEditFees';
+import AdminCategoryEdit from './Admin/AdminCategoryEdit';
+import AdminManager from './Admin/AdminManager';
+import AdminSellerWallet from './Admin/AdminSellerWallet';
+import AdminProSellerRequests from './Admin/AdminProSellerRequests'
 
 import HowItWorks from './seller/HowItWorks';
 import Wallet from './seller/Wallet';
@@ -82,7 +82,7 @@ import ProductBump from './seller/ProductBump';
 import SellerTransactions from './seller/SellerTransactions';
 import ProSellerAnalytics from './seller/ProSellerAnalytics';
 import SellerChat from './seller/SellerChat';
-import AdminBumpedProducts from './admin/AdminBumpedProducts';
+import AdminBumpedProducts from './Admin/AdminBumpedProducts';
 import UserAgreement from './pages/UserAgreement';
 import BuyerProtectionPolicy from './pages/BuyerProtectionPolicy';
 import RefundPolicy from './pages/RefundPolicy';
